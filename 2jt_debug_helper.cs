95c45a6a3b537e33f88e0b50f7628a219bc8b060
2jt-dir <62421336+2jt-dir@users.noreply.github.com>
2020-03-31 11:28:44 +0800

    Add files via upload

---
+++ 2jt_debug_helper.cs
@@ -0,0 +1,29 @@
+#nullable enable
+
+using System;
+using System.IO;
+
+namespace jt2.debug_helper_extensions
+{
+    static class Object_extensions
+    {
+        static string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public static void DumpObject(this object x, string? prepend = null, string? postscript = null) {
+            var jsonString = prepend + Newtonsoft.Json.JsonConvert.SerializeObject(x);
+            var outputString = (prepend ?? "") + jsonString;
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt"),true))
+            {
+                outputFile.WriteLine(outputString);
+                if (postscript is object) outputFile.WriteLine(postscript+"\n");
+            }
+        }
+        public static void dumpString(this string xString, string? prepend = null, string? postscript = null) {
+            var outputString = (prepend ?? "") + xString;
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt"),true))
+            {
+                outputFile.WriteLine(outputString);
+                if (postscript is object) outputFile.WriteLine(postscript+"\n");
+            }
+        }
+    }
+}

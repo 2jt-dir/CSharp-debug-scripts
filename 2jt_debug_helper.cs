2c0454907e2d0d2111591c6d122fc1f95685d3f0
2jt-dir <62421336+2jt-dir@users.noreply.github.com>
2020-03-31 11:55:38 +0800

    Update 2jt_debug_helper.cs

--- 2jt_debug_helper.cs
+++ 2jt_debug_helper.cs
@@ -8,23 +8,23 @@ namespace jt2.debug_helper_extensions
     static class Object_extensions
     {
         static string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-        
+
         public static void DumpObject(this object x, string? prepend = null, string? postscript = null) {
             var jsonString = prepend + Newtonsoft.Json.JsonConvert.SerializeObject(x);
             var outputString = (prepend ?? "") + jsonString;
             using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt"),true))
             {
                 outputFile.WriteLine(outputString);
-                if (postscript is object) outputFile.WriteLine(postscript+"\n");
+                if (postscript is object) outputFile.WriteLine(postscript);
             }
         }
-        
+
         public static void dumpString(this string xString, string? prepend = null, string? postscript = null) {
             var outputString = (prepend ?? "") + xString;
             using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "WriteLines.txt"),true))
             {
                 outputFile.WriteLine(outputString);
-                if (postscript is object) outputFile.WriteLine(postscript+"\n");
+                if (postscript is object) outputFile.WriteLine(postscript);
             }
         }
     }
